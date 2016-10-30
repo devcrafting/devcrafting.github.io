2c37d51ef36f158df614a974d97bca1e2a68c4a9
Clément Bouillier <clement.bouillier@devcrafting.com>
2016-10-30 22:27:04 +0100

    Use Unquote instead of Xunit assertions

--- tools/tools.tests/document.tests.fs
+++ tools/tools.tests/document.tests.fs
@@ -3,9 +3,9 @@ module Document.Tests
 open Domain
 open Document
 
-open Xunit
 open FsCheck
 open FsCheck.Xunit
+open Swensen.Unquote
 
 open System
 
@@ -19,11 +19,22 @@ let ``File with .md extension are transformed in an Article`` cfg =
         { cfg with
             SourceDir = "source/" 
             FileToUrlConvertionPatterns = ([], snd cfg.FileToUrlConvertionPatterns) }
+
     match transform cfg "source/minimal.md" with
-    | Article (_, _) -> true
-    | _ -> false
+    | Article (_, article) ->
+        article.Title =! "minimal"
+        article.Language =! "en"
+        article.ShortTitle =! "minimal"
+        article.Abstract =! "\n\n"
+        article.Body =! "\n\n"
+        article.Date =! DateTime.MinValue
+        test <@ not article.Hidden @>
+        article.Tags =! []
+        article.Type =! None
+        article.Layout =! "default"
+        article.RedirectFrom =! []
+    | _ -> test <@ false @>
 
 [<Property>]
 let ``File with .md extension transformation throws Exception if no metadata`` cfg =
-    let e = Assert.Throws<Exception>(fun () -> transform cfg "source/nometadata.md" |> ignore)
-    e.Message.StartsWith("No metadata")
+    raisesWith <@ transform cfg "source/nometadata.md" @> (fun e -> <@ e.Message.StartsWith("No metadata") @>)
