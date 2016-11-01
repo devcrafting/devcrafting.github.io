d848238a52085625e7a72699876dc46a407bf1a4
Clément Bouillier <clement.bouillier@devcrafting.com>
2016-11-01 11:00:55 +0100

    Test shortTitle metadata + reorganize tests (+ file renaming on Windows)

--- tools/tools.tests/document.tests.fs
+++ tools/tools.tests/document.tests.fs
@@ -9,32 +9,47 @@ open Swensen.Unquote
 
 open System
 
-[<Property>]
-let ``Any file with extension other than .fsx or .md are transformed in a Content file`` cfg =
-    transform cfg "anyfile" = Content "anyfile"
+let basicCfg cfg = 
+            { cfg with
+                SourceDir = "source/" 
+                FileToUrlConvertionPatterns = ([], snd cfg.FileToUrlConvertionPatterns) }
 
-[<Property>]
-let ``File with .md extension are transformed in an Article file`` cfg =
-    let cfg = 
-        { cfg with
-            SourceDir = "source/" 
-            FileToUrlConvertionPatterns = ([], snd cfg.FileToUrlConvertionPatterns) }
-
-    match transform cfg "source/minimal.md" with
-    | Article (_, article) ->
-        article.Title =! "minimal"
-        article.Language =! "en"
-        article.ShortTitle =! "minimal"
-        article.Abstract =! "\n\n"
-        article.Body =! "\n\n"
-        article.Date =! DateTime.MinValue
-        test <@ not article.Hidden @>
-        article.Tags =! []
-        article.Type =! None
-        article.Layout =! "default"
-        article.RedirectFrom =! []
+let (!=!) article testExpr = 
+    match article with
+    | Article (_, article) -> testExpr article
     | _ -> test <@ false @>
 
-[<Property>]
-let ``File with .md extension transformation throws Exception if no metadata`` cfg =
-    raisesWith <@ transform cfg "source/nometadata.md" @> (fun e -> <@ e.Message.StartsWith("No metadata") @>)
+module ``Simplest file transformations use case: `` =
+    [<Property>]
+    let ``Any file with extension other than .fsx or .md are transformed in a Content file`` cfg =
+        transform cfg "anyfile" = Content "anyfile"
+
+    [<Property>]
+    let ``Minimal .md file (i.e at least a title) is transformed in an Article file with default properties`` cfg =
+        let cfg = basicCfg cfg
+        transform cfg "source/minimal.md" !=!
+            fun article ->
+                article.UniqueKey =! ""
+                article.Title =! "minimal"
+                article.Language =! "en"
+                article.ShortTitle =! "minimal"
+                article.Abstract =! "\n\n"
+                article.Body =! "\n\n"
+                article.Date =! DateTime.MinValue
+                test <@ not article.Hidden @>
+                article.Tags =! []
+                article.Type =! None
+                article.Layout =! "default"
+                article.RedirectFrom =! []
+
+    [<Property>]
+    let ``Transformation of a .md file without at least a title in first line throws Exception`` cfg =
+        raisesWith <@ transform cfg "source/nometadata.md" @> (fun e -> <@ e.Message.StartsWith("No metadata") @>)
+
+module ``Transformation of 'basic' metadata: `` =
+
+    [<Property>]
+    let ``ShortTitle metadata is parsed`` cfg =
+        let cfg = basicCfg cfg
+        transform cfg "source/shortTitle.md" !=!
+            fun article -> article.ShortTitle =! "shortTitle"
